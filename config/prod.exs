f3797ab142d478108c614f41a18da3091f4e517a
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2020-11-06 19:40:39 +0100

    Add Gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rearranger, RearrangerWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "rearranger.gigalixirapp.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
